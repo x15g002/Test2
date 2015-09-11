ad657336a744c3bedcc5b8a241d928b639e5bee6
unknown <x15g002@PC505N010.chiba-fjb.ac.jp>
2015-09-11 11:51:43 +0900

    変更

--- src/Test01.java
+++ src/Test01.java
@@ -3,7 +3,9 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		System.out.println("ああわあ");
-		
+		System.out.println("ああわあ");
+		System.out.println("ああわあ");
+		System.out.println("ああわあ");
 
 	}
 
